6f875b78fbc6803fe665572d9ce37a14a67f6241
Dillon J. Choi <dillonchoi1992@gmail.com>
2022-03-09 18:08:46 -0500

    first commit

---
+++ flappyBird/flappyBird/MainWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace flappy_bird_clone
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        DispatcherTimer gameTimer = new DispatcherTimer();
+
+        double score;
+        int gravity = 8;
+        bool gameOver;
+        Rect flappyBirdHitBox;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            gameTimer.Tick += MainEventTimer;
+            gameTimer.Interval = TimeSpan.FromMilliseconds(20);
+            StartGame();
+        }
+
+        private void MainEventTimer(object? sender, EventArgs e)
+        {
+            txtScore.Content = $"Score: {score}";
+
+            flappyBirdHitBox = new Rect(Canvas.GetLeft(flappyBird), Canvas.GetTop(flappyBird), flappyBird.Width, flappyBird.Height);
+
+            Canvas.SetTop(flappyBird, Canvas.GetTop(flappyBird) + gravity);
+        }
+
+        private void KeyisDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+            {
+                flappyBird.RenderTransform = new RotateTransform(-20, flappyBird.Width / 2, flappyBird.Height / 2);
+                gravity = -8;
+            }
+
+            if (e.Key == Key.R && gameOver == true)
+            {
+                StartGame();
+            }
+        }
+
+        private void KeyisUp(object sender, KeyEventArgs e)
+        {
+            //if space is released, reset bird 
+            flappyBird.RenderTransform = new RotateTransform(5, flappyBird.Width / 2, flappyBird.Height / 2);
+            gravity = 8;
+
+        }
+
+        private void StartGame()
+        {
+            MyCanvas.Focus();
+
+            int temp = 300;
+
+            score = 0;
+
+            gameOver = false;
+
+            Canvas.SetTop(flappyBird, 150);
+
+            foreach (var x in MyCanvas.Children.OfType<Image>())
+            {
+                if ((string)x.Tag == "obs1")
+                {
+                    Canvas.SetLeft(x, 500);
+                }
+                if ((string)x.Tag == "obs2")
+                {
+                    Canvas.SetLeft(x, 800);
+                }
+                if ((string)x.Tag == "obs3")
+                {
+                    Canvas.SetLeft(x, 1100);
+                }
+
+                if ((string)x.Tag == "cloud")
+                {
+                    Canvas.SetLeft(x, 300 + temp);
+                    temp = 800;
+                }
+            }
+
+            gameTimer.Start();
+
+
+
+        }
+
+        private void EndGame()
+        {
+
+        }
+
+
+    }
+}
